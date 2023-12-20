1b180037847f856800703ace30342c31d34697e8
Haxodius <murofurki2002@hotmail.com>
2023-12-20 23:12:49 +0300

    konsol 6 implemented fully

--- Konsol6/src/main.py
+++ Konsol6/src/main.py
@@ -1,40 +1,55 @@
 import cv2
 import numpy as np
+for i in range(32):
+    image_org = cv2.imread(f'../input/{i+1}.jpg', cv2.IMREAD_GRAYSCALE)
 
-image_org = cv2.imread('Konsol6/src/input/blob-test.png', cv2.IMREAD_GRAYSCALE)
+    height = image_org.shape[0]
+    width = image_org.shape[1]
 
-image = cv2.GaussianBlur(image_org, (5, 5), 0)
-image = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)[1]
-kernel = np.ones((3, 3), np.uint8)
-image = cv2.dilate(image, kernel, iterations=2)
-image = cv2.erode(image, kernel, iterations=1)
+    #(width, height)
+    if height == 4032:
+        image_org = cv2.resize(image_org, (600, 802))
 
-params = cv2.SimpleBlobDetector_Params()
-params.minThreshold = 10
-params.maxThreshold = 255
-params.filterByArea = True
-params.minArea = 500
-params.maxArea = 1125000
-params.filterByCircularity = True
-params.minCircularity = 0.85
+    else:
+        image_org = cv2.resize(image_org, (802, 600))
 
-detector = cv2.SimpleBlobDetector_create(params)
+    image = cv2.GaussianBlur(image_org, (51, 51), 0)
+    #192, 193
+    image = cv2.threshold(image, 192, 193, cv2.THRESH_BINARY_INV)[1]
+    #kernel = np.ones((3, 3), np.uint8)
+    #image = cv2.dilate(image, kernel, iterations=2)
+    #image = cv2.erode(image, kernel, iterations=1)
 
-keypoints = detector.detect(image)
+    params = cv2.SimpleBlobDetector_Params()
+    #params.minThreshold = 10
+    #params.maxThreshold = 255
+    params.filterByArea = True
+    params.minArea = 300
+    params.maxArea = 1125000
+    params.filterByCircularity = True
+    params.minCircularity = 0.40
 
-image_keypoints = np.zeros_like(image_org)
-image_keypoints = cv2.drawKeypoints(image_org, keypoints, image_keypoints, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    detector = cv2.SimpleBlobDetector_create(params)
 
-blobs_only_image = np.zeros_like(image_org)
-blobs_only_image = cv2.drawKeypoints(blobs_only_image, keypoints, blobs_only_image, (255, 255, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    keypoints = detector.detect(image)
 
-for kp in keypoints:
-    x, y = np.int0(kp.pt)
-    cv2.circle(image_keypoints, (x, y), int(kp.size/2), (255, 0, 0), thickness=2)
-    cv2.circle(blobs_only_image, (x, y), int(kp.size/2), (255, 255, 255), thickness=2)
+    image_keypoints = np.zeros_like(image_org)
+    image_keypoints = cv2.drawKeypoints(image_org, keypoints, image_keypoints, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-cv2.imshow("Original Image", image_org)
-cv2.imshow("Keypoints", image_keypoints)
-cv2.imshow("Blobs Only", blobs_only_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    blobs_only_image = np.zeros_like(image_org)
+    blobs_only_image = cv2.drawKeypoints(blobs_only_image, keypoints, blobs_only_image, (255, 255, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    cv2.imwrite(f"../output/{i+1}_output.jpg", image_keypoints)
+
+    """
+    for kp in keypoints:
+        x, y = np.int0(kp.pt)
+        cv2.circle(image_keypoints, (x, y), int(kp.size/2), (255, 0, 0), thickness=2)
+        cv2.circle(blobs_only_image, (x, y), int(kp.size/2), (255, 255, 255), thickness=2)
+    
+    cv2.imshow("Original Image", image_org)
+    cv2.imshow("Keypoints", image_keypoints)
+    cv2.imshow("Blobs Only", blobs_only_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    """
