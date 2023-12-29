36e02ff788eee127f8158cd87f3fd467c70633e2
Muhammed Emin Çağlar <100859025+Caglaar@users.noreply.github.com>
2023-12-29 12:26:24 +0300

    added-new-color-group-and-change-found-robot-fuction

--- Konsol2/src/main.py
+++ Konsol2/src/main.py
@@ -10,45 +10,31 @@ def get_number_from_filename(filename):
         return int(file_number)
 def get_color_from_file(number):
     if(number >= 1 and number <= 2):
-        return np.array([[24,14,56], [87,91,175]])
+        return np.array([[24,16,56], [87,91,171]])
     elif(number >= 3 and number <= 4):
-        return np.array([[59,28,114], [89,94,163]])
-    elif(number == 5):
-        return np.array([[30,25,45], [80,97,138]])
-    elif(number == 6):
-        return np.array([[27,30,25], [80,160,142]])
-    elif(number >= 7 and number <= 8):
-        return np.array([[30,28,33], [97,155,160]])
-    elif(number >= 9 and number <= 10):
-        return np.array([[67,48,58], [97,102,255]])
-    elif(number >= 11 and number <= 12):
-        return np.array([[41,39,45], [97,87,255]])
+        return np.array([[59,30,95], [89,94,163]])
+    elif(number >= 5 and number <= 8):
+        return np.array([[30,30,34], [89,142,166]])
+    elif(number >= 9 and number <= 12):
+        return np.array([[54,47,50], [98,94,255]])
     elif(number >= 13 and number <= 14):
-        return np.array([[38,53,117], [56,101,255]])
-    elif(number >= 15 and number <= 17):
-        return np.array([[0,31,45], [64,95,255]])
-    elif(number >= 18 and number <= 22):
-        return np.array([[0,40,111], [66,85,255]])
-    elif(number >= 23 and number <= 24):
-        return np.array([[31,20,106], [78,69,255]])
-    elif(number >= 25 and number <= 27):
-        return np.array([[33,11,116], [80,58,255]])
+        return np.array([[38,53,117], [56,100,255]])
+    elif(number >= 15 and number <= 22):
+        return np.array([[0,36,81], [66,110,255]])
+    elif(number >= 23 and number <= 27):
+        return np.array([[30,16,113], [80,64,255]])
     elif(number >= 28 and number <= 49):
-        return np.array([[75,25,165], [100,55,255]])
-    elif(number >= 50 and number <= 62):
-        return np.array([[47,25,83], [100,90,255]])
-    elif(number >= 63 and number <= 69):
-        return np.array([[42,5,83], [107,75,170]])
+        return np.array([[78,17,169], [99,55,255]])
+    elif(number >= 50 and number <= 69):
+        return np.array([[44,16,86], [105,78,212]])
     elif(number >= 70 and number <= 79):
-        return np.array([[50,0,160], [179,53,255]])
+        return np.array([[64,3,163], [150,50,255]])
     elif(number >= 80 and number <= 89):
-        return np.array([[20,0,11], [132,130,74]])
+        return np.array([[20,0,11], [139,131,75]])
     elif(number >= 90 and number <= 94):
-        return np.array([[65,0,45], [131,124,171]])
-    elif(number >= 95 and number <= 99):
-        return np.array([[100,0,53], [141,120,181]])
-    elif(number >= 100 and number <= 108):
-        return np.array([[100,20,63], [122,125,153]])
+        return np.array([[65,0,42], [131,150,190]])
+    elif(number >= 95 and number <= 108):
+        return np.array([[100,11,56], [132,122,166]])
     else:
         return None
 
@@ -88,16 +74,13 @@ for image_file in image_files:
 
     #Her bir robotu işaretle
     if len(filtered_contours) > 0:
-        # En büyük alana sahip konturu bul
-        largest_contour = max(filtered_contours, key=cv2.contourArea)
-
-        # Robotu işaretleme ve etiketleme işlemleri
-        M = cv2.moments(largest_contour)
+       for cnt in filtered_contours:  # Her konturu dolaş
+        M = cv2.moments(cnt)
         cx = int(M['m10'] / M['m00'])
         cy = int(M['m01'] / M['m00'])
-        area = cv2.contourArea(largest_contour)
+        area = cv2.contourArea(cnt)
 
-        cv2.drawContours(image, [largest_contour], -1, (0, 0, 255), 2)
+        cv2.drawContours(image, [cnt], -1, (0, 0, 255), 2)
         cv2.putText(image, "Robot", (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
     else:
         print("Hiç robot bulunamadı.")
