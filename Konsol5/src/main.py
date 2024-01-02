337f7a28516aa57f0181511ac5900a8efff29e14
ResulSalci <rusturesulsalci@gmail.com>
2024-01-03 01:31:51 +0300

    argparse entegrated.

--- Konsol5/src/main.py
+++ Konsol5/src/main.py
@@ -1,49 +1,52 @@
 import cv2
 import os
 import numpy as np
+import argparse
 
 
-def tespit_ve_ciz(img_path, output_path):
-    # Resmi oku
-    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+def tespit_ve_ciz(args):
+    # Input klasörü ve output klasörü
+    input_folder = "../input"
+    output_folder = "../output"
 
-    # Gri tonlamaya çevir
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Input klasöründeki her resmi işle
+    for image_file in os.listdir(input_folder):
+        # Görüntüyü oku
+        img = cv2.imread(os.path.join(input_folder, image_file), cv2.IMREAD_COLOR)
 
-    # Gürültüyü azaltmak için Gaussian bulanıklaştırma uygula
-    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+        # Gri tonlamaya çevir
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Hough Circle yöntemi ile çemberleri tespit et
-    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1, minDist=100,
-                               param1=75, param2=40, minRadius=95, maxRadius=190)
+        # Gürültüyü azaltmak için Gaussian bulanıklaştırma uygula
+        blurred = cv2.GaussianBlur(gray, (3, 3), 0)
 
-    if circles is not None:
-        # Çemberleri çiz
-        circles = np.uint16(np.around(circles))
-        for i in circles[0, :]:
-            # Çemberin merkezini çiz
-            cv2.circle(img, (i[0], i[1]), 2, (0, 0, 0), 9)
-            # Çembiri çiz
-            cv2.circle(img, (i[0], i[1]), i[2], (0, 0, 255), 3)
+        # Hough Circle yöntemi ile çemberleri tespit et
+        circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1, minDist=args.minDist,
+                                   param1=args.param1, param2=args.param2, minRadius=args.minRadius, maxRadius=args.maxRadius)
 
-        # İşlenmiş resmi kaydet
-        output_file = os.path.join(output_path, os.path.basename(img_path))
-        cv2.imwrite(output_file, img)
-        print(f"{output_file} dosyası başarıyla oluşturuldu.")
-    else:
-        print("Çember bulunamadı.")
+        if circles is not None:
+            # Çemberleri çiz
+            circles = np.uint16(np.around(circles))
+            for i in circles[0, :]:
+                # Çemberin merkezini çiz
+                cv2.circle(img, (i[0], i[1]), 2, (0, 0, 0), 9)
+                # Çemberi çiz
+                cv2.circle(img, (i[0], i[1]), i[2], (0, 0, 255), 3)
+
+            # İşlenmiş resmi kaydet
+            cv2.imwrite(os.path.join(output_folder, image_file), img)
 
 
-# Input klasörü ve output klasörü
-input_klasoru = "../input"
-output_klasoru = "../output"
+if __name__ == '__main__':
 
-# Output klasörünü oluştur
-if not os.path.exists(output_klasoru):
-    os.makedirs(output_klasoru)
+    parser = argparse.ArgumentParser(description='Çember tespit etme parametreleri')
+    parser.add_argument('minDist', type=int, default=100, nargs='?')
+    parser.add_argument('param1', type=int, default=75, nargs='?')
+    parser.add_argument('param2', type=int, default=40, nargs='?')
+    parser.add_argument('minRadius', type=int, default=95, nargs='?')
+    parser.add_argument('maxRadius', type=int, default=190, nargs='?')
 
-# Input klasöründeki her resmi işle
-for file_name in os.listdir(input_klasoru):
-    if file_name.endswith((".jpg", ".jpeg", ".png")):
-        img_path = os.path.join(input_klasoru, file_name)
-        tespit_ve_ciz(img_path, output_klasoru)
+    args = parser.parse_args()
+
+    tespit_ve_ciz(args)
+
